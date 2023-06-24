bc1315e52b622122c2b14b9a6a3c1316895842af
Jon Mico <jonmico@gmail.com>
2023-06-24 12:48:58 -0400

    Add key to FormSplitBill.

--- src/App.tsx
+++ src/App.tsx
@@ -88,6 +88,7 @@ export default function App() {
       </div>
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
